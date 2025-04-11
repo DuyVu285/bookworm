8c11a40b3c6b9b8c0e1a035866a8877d07056c87
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-04-11 16:17:14 +0700

    feat: Init database connection to PostgreSQL

---
+++ backend/app/db/db.py
@@ -0,0 +1,16 @@
+from app.core.config import settings
+from sqlmodel import SQLModel, create_engine, Session
+
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
