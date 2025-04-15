016d2340460e3b5c9a4ef6efa7dc2a76f66772f3
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-04-15 10:19:47 +0700

    create book route

--- backend/app/api/routes/book_route.py
+++ backend/app/api/routes/book_route.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, select
 
 from app.db.db import get_session
 from app.models.book_model import Book
-from app.schemas.book_schema import BookBase
+from app.schemas.book_schema import BookBase, BookCreate, BookUpdate, BookRead
 
 router = APIRouter(
     prefix="/books", tags=["books"], responses={404: {"description": "Not found"}}
@@ -21,7 +21,7 @@ async def get_books(session: Session = Depends(get_session)):
     return books
 
 
-@router.get("/{book_id}", response_model=BookBase, status_code=status.HTTP_200_OK)
+@router.get("/{book_id}", response_model=BookRead, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int, session: Session = Depends(get_session)):
     book = session.get(Book, book_id)
     if not book:
@@ -31,10 +31,40 @@ async def get_book(book_id: int, session: Session = Depends(get_session)):
     return book
 
 
-@router.post("/", response_model=BookBase, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=BookCreate, status_code=status.HTTP_201_CREATED)
 async def create_book(book: BookBase, session: Session = Depends(get_session)):
     new_book = Book.model_validate(book)
     session.add(new_book)
     session.commit()
     session.refresh(new_book)
     return new_book
+
+
+@router.put("/{book_id}", response_model=BookUpdate, status_code=status.HTTP_200_OK)
+async def update_book(
+    book_id: int, book: BookUpdate, session: Session = Depends(get_session)
+):
+    db_book = session.get(Book, book_id)
+    if not db_book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
+        )
+    update_data = book.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_book, key, value)
+    session.add(db_book)
+    session.commit()
+    session.refresh(db_book)
+    return db_book
+
+
+@router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int, session: Session = Depends(get_session)):
+    book = session.get(Book, book_id)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
+        )
+    session.delete(book)
+    session.commit()
+    return
--- backend/app/schemas/book_schema.py
+++ backend/app/schemas/book_schema.py
@@ -1,19 +1,23 @@
 from pydantic import BaseModel, ConfigDict
 
+
 class BookBase(BaseModel):
     book_title: str
     book_summary: str
     book_price: float
     book_cover_photo: str
 
+
 class BookCreate(BookBase):
     category_id: int
     author_id: int
-    
+
+
 class BookUpdate(BookBase):
     pass
 
+
 class BookRead(BookBase):
     id: int
-    
+
     model_config = ConfigDict(from_attributes=True)
