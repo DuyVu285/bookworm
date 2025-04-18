da8ddd7d4929d7a1b27ef93957a9088d2dd8cbb5
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-04-18 10:21:49 +0700

    add auth handler

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,9 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     
     model_config = {
         "env_file": ".env",
