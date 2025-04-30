847f2e8fe0b30dd1a79fa5f8e94f5d5ebfaac743
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2025-04-30 23:10:17 +0700

    minor responsive change on swiper

--- frontend/src/sections/HomeSection/OnSale.tsx
+++ frontend/src/sections/HomeSection/OnSale.tsx
@@ -90,7 +90,7 @@ const OnSale = () => {
               slidesPerView={"auto"}
               breakpoints={{
                 320: { slidesPerView: 1, spaceBetween: 10 },
-                640: { slidesPerView: 1, spaceBetween: 20 },
+                640: { slidesPerView: 2, spaceBetween: 20 },
                 1024: { slidesPerView: 4, spaceBetween: 50 },
               }}
               className="flex-1"
