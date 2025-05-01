0645f747cecc6937fd4971565830c2d4f22f8cd7
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2025-05-01 17:41:46 +0700

    css fix

--- frontend/src/components/BookGridDisplay.tsx
+++ frontend/src/components/BookGridDisplay.tsx
@@ -24,7 +24,7 @@ const BookGridDisplay = ({
         loadingMessage
       ) : books.length > 0 ? (
         <div
-          className={`grid gap-8 w-full justify-center xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4`}
+          className={`grid gap-8 w-full justify-center xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4`}
         >
           {books.map((book) => (
             <BookCard key={book.id} {...book} />
