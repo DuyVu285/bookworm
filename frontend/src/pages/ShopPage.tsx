b5b72e0ccb34089887ccaf0c619b4858bd7b5d3f
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2025-05-02 13:03:14 +0700

    temp update breadcrumbs

--- frontend/src/pages/ShopPage.tsx
+++ frontend/src/pages/ShopPage.tsx
@@ -1,10 +1,20 @@
 import BooksGrid from "../sections/ShopSection/BooksGrid";
 import Filters from "../sections/ShopSection/Filters";
 import MainLayout from "../components/layout/MainLayout";
+import { FILTER_KEYS, useQueryFilters } from "../hooks/useQueryFilters";
 
 const ShopPage = () => {
+  const { getParam } = useQueryFilters();
+
+  const category = getParam(FILTER_KEYS.CATEGORY);
+  const author = getParam(FILTER_KEYS.AUTHOR);
+  const rating = getParam(FILTER_KEYS.RATING);
+
+  const filters = [category, author, rating].filter(Boolean);
+  const displayString = filters.length > 0 ? `${filters.join(", ")}` : "";
+
   return (
-    <MainLayout type="Books">
+    <MainLayout type="Books" value={displayString}>
       <div className="flex flex-col lg:flex-row mx-18">
         {/* Aside Filters */}
         <aside className="w-full lg:w-1/6">
